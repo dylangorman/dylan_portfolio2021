9ec508699987f7183828f88b4be85c572d2058ef
dylangorman <dylangorman@me.com>
2021-10-19 23:20:14 +0100

    second commit

---
+++ music.js
@@ -0,0 +1,13 @@
+var audio = document.getElementById('audio');
+        var playpause = document.getElementById("play");
+        
+        
+        function togglePlayPause() {
+           if (audio.paused || audio.ended) {
+              playpause.title = "Pause";
+              audio.play();
+           } else {
+              playpause.title = "Play";
+              audio.pause();
+           }
+        }
